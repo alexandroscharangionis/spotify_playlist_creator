534b284f3084ef67e405ee83019764aa23a1fb6d
alexandroscharangionis <alexandroscharangionis@gmail.com>
2023-01-31 18:10:23 +0200

    spotify API setup

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 from bs4 import BeautifulSoup
 import requests
+import spotipy
+from spotipy.oauth2 import SpotifyOAuth
 
 
 date = input(
@@ -19,5 +21,23 @@ all_song_writers = soup.select("span.a-no-trucate")
 song_titles = [record.getText().strip() for record in all_song_titles]
 song_writers = [record.getText().strip() for record in all_song_writers]
 
-print(song_titles)
-print(song_writers)
+# print(song_titles)
+# print(song_writers)
+
+# SPOTIFY API SETUP
+
+SPOTIFY_ID = "d0e50d9cb9ca4a3f88bb4ceb1cdc71ad"
+SPOTIFY_SECRET = ""
+
+sp = spotipy.Spotify(
+    auth_manager=SpotifyOAuth(
+        scope="playlist-modify-private",
+        redirect_uri="http://localhost:8888/callback",
+        client_id=SPOTIFY_ID,
+        client_secret=SPOTIFY_SECRET,
+        show_dialog=True,
+        cache_path="token.txt"
+    )
+)
+
+user_id = sp.current_user()["id"]
